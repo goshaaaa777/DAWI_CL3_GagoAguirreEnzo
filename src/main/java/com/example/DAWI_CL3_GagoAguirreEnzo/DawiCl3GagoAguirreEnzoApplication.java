0ff9a16e508bf8d29a999dfd9e79fb08f6dc8c14
goshaaaa777 <isaiasddx322z@gmail.com>
2023-11-26 10:19:29 -0500

    Initial commit

---
+++ src/main/java/com/example/DAWI_CL3_GagoAguirreEnzo/DawiCl3GagoAguirreEnzoApplication.java
@@ -0,0 +1,13 @@
+package com.example.DAWI_CL3_GagoAguirreEnzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3GagoAguirreEnzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3GagoAguirreEnzoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DAWI_CL3_GagoAguirreEnzo/DawiCl3GagoAguirreEnzoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DAWI_CL3_GagoAguirreEnzo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl3GagoAguirreEnzoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
